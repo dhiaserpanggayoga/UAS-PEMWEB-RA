baa2a661ba9fe32e2f984a79da104f416821ba1d
dhias erpangga yoga <145907559+dhiaserpanggayoga@users.noreply.github.com>
2025-05-28 14:02:05 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import AppRouter from './routes/AppRouter';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <AppRouter />
+    </>
+  );
+}
+
+export default App;
