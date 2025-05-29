30ea7c126b17f1797adf19cd57333162b69a1fcd
dhias erpangga yoga <145907559+dhiaserpanggayoga@users.noreply.github.com>
2025-05-29 17:43:30 +0700

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,61 +1,20 @@
-import os
-
 from setuptools import setup, find_packages
-
-here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.txt')) as f:
-    README = f.read()
-with open(os.path.join(here, 'CHANGES.txt')) as f:
-    CHANGES = f.read()
-
-requires = [
-    'alembic',
-    'plaster_pastedeploy',
-    'pyramid >= 1.9',
-    'pyramid_debugtoolbar',
-    'pyramid_jinja2',
-    'pyramid_retry',
-    'pyramid_tm',
-    'SQLAlchemy',
-    'transaction',
-    'zope.sqlalchemy',
-    'waitress',
-]
-
-tests_require = [
-    'WebTest >= 1.3.1',  # py3 compat
-    'pytest>=3.7.4',
-    'pytest-cov',
-]
 
 setup(
     name='resep_dapur',
-    version='0.0',
-    description='resep_dapur',
-    long_description=README + '\n\n' + CHANGES,
-    classifiers=[
-        'Programming Language :: Python',
-        'Framework :: Pyramid',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+    version='0.1',
+    packages=find_packages(),  # otomatis cari semua package di folder resep_dapur
+    install_requires=[
+        'pyramid',
+        'SQLAlchemy',
+        'zope.sqlalchemy',
+        'pyramid_jinja2',
+        'waitress',
+        # tambahkan dependencies lain sesuai kebutuhan
     ],
-    author='',
-    author_email='',
-    url='',
-    keywords='web pyramid pylons',
-    packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False,
-    extras_require={
-        'testing': tests_require,
-    },
-    install_requires=requires,
     entry_points={
         'paste.app_factory': [
             'main = resep_dapur:main',
-        ],
-        'console_scripts': [
-            'initialize_resep_dapur_db = resep_dapur.scripts.initialize_db:main',
         ],
     },
 )
